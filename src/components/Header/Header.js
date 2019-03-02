01e5db2a4b47420cb99e2be8e51e06f605b78e87
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2019-03-02 23:32:36 +0300

    add show different menu after succesful login.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Menu, Image, Divider, Button,
+  Menu, Image, Divider, Button, Dropdown,
 } from 'semantic-ui-react';
 
 import './Header.css';
@@ -18,16 +18,35 @@ const AppHeader = () => (
           <Image src={logo} width="100px" />
         </Link>
       </Menu.Item>
+      {
+          localStorage.getItem('access_token')
+            ? (
+              <Menu.Item className="header-right">
+                <Button color="green">Yaz</Button>
+                &nbsp;
+                &nbsp;
+                &nbsp;
+                <Dropdown button text="Profil">
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Ayarlar</Dropdown.Item>
+                    <Dropdown.Item onClick={() => { localStorage.clear(); window.location.replace('/'); }}>Çıkış</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Menu.Item>
+            )
+            : (
+              <Menu.Item className="header-right">
+                <LogIn button={<Button basic>Giriş Yap</Button>} />
+                &nbsp;
+                &nbsp;
+                yada
+                &nbsp;
+                &nbsp;
+                <Register button={<Button basic color="green">Kayıt Ol</Button>} />
+              </Menu.Item>
+            )
+        }
 
-      <Menu.Item className="header-right">
-        <LogIn button={<Button basic>Giriş Yap</Button>} />
-      </Menu.Item>
-
-      <Menu.Item className="header-right">yada</Menu.Item>
-
-      <Menu.Item className="header-right">
-        <Register button={<Button basic color="green">Kayıt Ol</Button>} />
-      </Menu.Item>
     </Menu>
     <Divider />
   </div>
