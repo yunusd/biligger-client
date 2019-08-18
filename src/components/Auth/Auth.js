91fa23512b67d4f5655a88febd7c9abade351ab7
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2019-08-18 11:49:35 +0300

    fix getMe undefined issue

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { useApolloClient, useQuery } from 'react-apollo-hooks';
 import GET_ME from './queries';
 
-const Auth = props => (
-  <Query query={GET_ME}>
-    {({
-      data,
-      loading,
-      error,
-      client,
-    }) => {
-      if (loading) return '';
-      if (error) return <div {...props} />;
-      client.writeData({
-        data: {
-          currentUser: {
-            __typename: 'User',
-            isLoggedIn: !!data.getMe.username || false,
-          },
-        },
-      });
-      return <div {...props} />;
-    }}
-  </Query>
-  );
+const Auth = (props) => {
+  const client = useApolloClient();
+  const {
+    data, loading, error,
+  } = useQuery(GET_ME);
+  if (loading) return null;
+  if (error) return <div {...props} />;
+
+  client.writeData({
+    data: {
+      currentUser: {
+        __typename: 'User',
+        isLoggedIn: !!data.getMe.username || false,
+      },
+    },
+  });
+  return <div {...props} />;
+};
 
 export default Auth;
